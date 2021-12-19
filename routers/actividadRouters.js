1f60ae061af9fc3326dfd4a910dd61ba0dc205bb
Prudencio Velasco <32465853+PrudencioVelasco@users.noreply.github.com>
2021-12-19 17:11:58 -0600

    v19122021
    Actividad Modificacion de ruta

--- routers/actividadRouters.js
+++ routers/actividadRouters.js
@@ -1,6 +1,6 @@
 const actividadesController = require('../controllers/actividadController'); 
 module.exports = app => {  
-    app.get("/monarca/ruta/todasActividades", actividadesController.todasActividades); 
+    app.get("/monarca/actividad/todasActividades", actividadesController.todasActividades); 
     app.post("/monarca/actividad/obtenerActividadLugar", actividadesController.obtenerActividadLugar); 
   
 }
